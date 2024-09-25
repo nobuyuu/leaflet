780aeee69ef51a613d7859d8523b8648be5f584b
nobuyuu <yu-ya_03_20_nobu@outlook.com>
2024-09-25 22:22:28 +0900

    popup

--- main.js
+++ main.js
@@ -3,19 +3,55 @@ let lng = 139.8108103; // 経度
 let zoom = 16; // ズームレベル
 
 let map = L.map("map"); // 地図の生成
-map.setView([lat, lng], zoom); // 緯度経度、ズームレベルを設定する
+map.setView([33.4943712, 130.5189245], 14); // 緯度経度、ズームレベルを設定する
 
 // タイルレイヤを生成し、地図に追加する
 // 今回はOpenStreetMapを表示する
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    // 著作物の表示
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }
+    {
+        // 著作物の表示
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
 ).addTo(map);
 
 // アイコンを表示する
 // lat, lingの順で指定する
-let marker = L.marker([35.709058, 139.8136911]).addTo(map);
+let marker = L.marker([33.4943712, 130.5189245]).addTo(map);
 // ポップアップを表示する
-marker.bindPopup("押上駅");
+marker.bindPopup("二日市駅");
+
+//　円を追加
+let circle = L.circle([33.5031989, 130.5065299], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+circle.bindPopup("都府楼南駅");
+
+// 三角形を表示
+let polygon = L.polygon([
+    [33.5083047, 130.5238172],
+    [33.4945121, 130.5353455],
+    [33.4959983, 130.5225263]
+]).addTo(map);
+
+polygon.bindPopup("△");
+
+
+let popup = L.popup()
+    .setLatLng([33.4943712, 130.5189245])
+    .setContent("ポップアップウィンドウ")
+    .openOn(map);
+
+//　クリックした箇所の経度緯度を表示
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("緯度経度" + e.latlng.toString())
+        .openOn(map);
+}
+
+
+map.on('click', onMapClick);
